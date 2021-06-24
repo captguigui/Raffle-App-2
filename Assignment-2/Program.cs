d2e7831db1715e9548621728bbe86fdc0273f27f
Guillermo Frenero <g.frenero.sanchez@gmail.com>
2021-06-24 12:56:58 -0400

    Added comments to further explain code.

--- Assignment-2/Program.cs
+++ Assignment-2/Program.cs
@@ -19,7 +19,7 @@ namespace ConsoleUI
             PrintWinner();
         }
 
-// private static variables to be accessed throughout the entire class (Added Regex for input validation)
+// private static variables to be accessed throughout the entire class (Added Regex for input validation).
         private static Dictionary<int, string> guests = new Dictionary<int, string>();
         private static int min = 1000;
         private static int max = 9999;
@@ -27,7 +27,7 @@ namespace ConsoleUI
         private static Random _rdm = new Random();
         private static Regex rgx = new Regex("[^a-zA-Z]");
 
-// Created a Regex method to trim and make inputs lowercase
+// Created a Regex method to trim and make inputs lowercase and return string.
         private static string Rgx(string input)
         {
             string user = input.Trim();
@@ -35,6 +35,8 @@ namespace ConsoleUI
             return user;
         }
 
+// GetUserInput method to prompt user to input a string of characters.
+// While loop will validate if string is empty or if characters are not repeated and trimmed of numbers.
         private static string GetUserInput(string message)
         {
             Console.WriteLine(message);
@@ -51,6 +53,9 @@ namespace ConsoleUI
             return user;
         }
 
+// GetUserInfo method that calls GetUserInput to get its information.
+// Do While loop to assign a raffle number to string entered by user by calling the GenerateRandomNumber method.
+// AddGuestsInRaffle method is also called in this loop to pass the string and integer given.
         private static void GetUserInfo()
         {
             string name;
@@ -69,17 +74,21 @@ namespace ConsoleUI
             } while (otherGuest == "yes");
         }
 
+// Random number generator method that produces a number between the min and max.
         private static int GenerateRandomNumber(int min, int max)
         {
             int randomNum = _rdm.Next(min, max);
             return randomNum;
         }
 
+// AddGuestsInRaffle method to take int and string from GetUserInfo and add it to Dictionary.
         private static void AddGuestsInRaffle(int raffleNumber, string guest)
         {
             guests.Add(raffleNumber, guest);
         }
 
+// Method to print name of guests entered along with their ticket number.
+// This is done with the foreach KeyValuePair loop.
         private static void PrintGuestsName()
         {
             Console.WriteLine("Guests and their ticket number:");
@@ -89,6 +98,8 @@ namespace ConsoleUI
             }
         }
 
+// Method to generate random ticket number by using the Random class with Dictionary's count.
+// Winner int is then assigned with random key value given from index.
         private static int GetRaffleNumber(Dictionary<int, string> people)
         {
             int index = _rdm.Next(guests.Count);
@@ -97,6 +108,9 @@ namespace ConsoleUI
             return winnerNumber;
         }
 
+// Method to print winner's number by calling the GetRaffleNumber method.
+// winnerName is then found by creating a new string and assigning it to the Dictionary's Value
+// that is paired with it's Key from the winning number.
         private static void PrintWinner()
         {
             int winnerNumber = GetRaffleNumber(guests);
